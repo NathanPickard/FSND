870594ed743b4878c07cd586044f09797d8f5715
Nathan Pickard <nathan.pickard87@gmail.com>
2020-06-13 21:56:03 -0700

    setup.sh, runtime.txt, and Procfile addition

--- projects/capstone/heroku_sample/starter/setup.sh
+++ projects/capstone/heroku_sample/starter/setup.sh
@@ -1,1 +1,2 @@
-web: gunicorn app:app
+DATABASE_URL=postgres://ywbbigmllispqd:01c821c6bebd85eb9ce4739bda2f19458ee9e866c4b13428929ee271bce4fc2b@ec2-52-70-15-120.compute-1.amazonaws.com:5432/de0u9so6qbap2
+EXCITED=true
